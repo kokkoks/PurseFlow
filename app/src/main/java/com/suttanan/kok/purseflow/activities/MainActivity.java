caddae4fe5063d16d905461ccb12c5f39922b755
kokkoks <kok_satan@hotmail.com>
2016-05-03 20:03:17 +0700

    add empty tabhost

--- app/src/main/java/com/suttanan/kok/purseflow/activities/MainActivity.java
+++ app/src/main/java/com/suttanan/kok/purseflow/activities/MainActivity.java
@@ -1,12 +1,12 @@
 package com.suttanan.kok.purseflow.activities;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TabHost;
 
 import com.suttanan.kok.purseflow.R;
 import com.suttanan.kok.purseflow.adapters.MyFragmentPagerAdapter;
@@ -14,18 +14,76 @@ import com.suttanan.kok.purseflow.fragments.HomeFragment;
 import com.suttanan.kok.purseflow.fragments.InformaionFragment;
 import com.suttanan.kok.purseflow.fragments.VisualizationFragment;
 
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends ActionBarActivity{
+public class MainActivity extends ActionBarActivity implements ViewPager.OnPageChangeListener, TabHost.OnTabChangeListener{
 
     ViewPager viewPager;
+    TabHost tabHost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViewPagers();
+        initTabHost();
+    }
+
+    private void initTabHost() {
+        tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        String[] tabNames = {"Home", "Information", "Visual"};
+
+        for(int i = 0; i < tabNames.length; i++){
+            TabHost.TabSpec tabSpec;
+            tabSpec = tabHost.newTabSpec(tabNames[i]);
+            tabSpec.setIndicator(tabNames[i]);
+            tabSpec.setContent(new FakeContent(getApplicationContext()));
+            tabHost.addTab(tabSpec);
+        }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onTabChanged(String tabId) {
+        
+    }
+
+    public class FakeContent implements TabHost.TabContentFactory{
+
+        Context context;
+
+        public FakeContent(Context mcontext) {
+            context = mcontext;
+        }
+
+        @Override
+        public View createTabContent(String tag) {
+            View fakeView = new View(context);
+            fakeView.setMinimumHeight(0);
+            fakeView.setMinimumWidth(0);
+            return fakeView;
+        }
+    }
+
+    private void initViewPagers(){
         viewPager = (ViewPager)findViewById(R.id.view_pager);
 
         List<Fragment> listFragments =  new ArrayList<Fragment>();
