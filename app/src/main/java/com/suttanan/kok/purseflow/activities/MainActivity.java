66e39f069fd44ddf4949c8eca55f62bc288190e7
kokkoks <kok_satan@hotmail.com>
2016-05-04 00:20:06 +0700

    delete tabhost code

--- app/src/main/java/com/suttanan/kok/purseflow/activities/MainActivity.java
+++ app/src/main/java/com/suttanan/kok/purseflow/activities/MainActivity.java
@@ -22,48 +22,8 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        initViewPagers();
-//        initTabHost();
         initTabs();
     }
-
-//    @Override
-//    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//
-//    }
-//
-//    @Override
-//    public void onPageSelected(int selectedPage) {
-//        tabHost.setCurrentTab(selectedPage);
-//    }
-//
-//    @Override
-//    public void onPageScrollStateChanged(int state) {
-//
-//    }
-//
-//    @Override
-//    public void onTabChanged(String tabId) {
-//        int selectedPage = tabHost.getCurrentTab();
-//        viewPager.setCurrentItem(selectedPage);
-//    }
-
-//    public class FakeContent implements TabHost.TabContentFactory{
-//
-//        Context context;
-//
-//        public FakeContent(Context mcontext) {
-//            context = mcontext;
-//        }
-//
-//        @Override
-//        public View createTabContent(String tag) {
-//            View fakeView = new View(context);
-//            fakeView.setMinimumHeight(0);
-//            fakeView.setMinimumWidth(0);
-//            return fakeView;
-//        }
-//    }
 
     private void initTabs() {
         ActionBar actionBar = getSupportActionBar();
@@ -80,32 +40,4 @@ public class MainActivity extends AppCompatActivity{
         actionBar.selectTab(homeTab);
 
     }
-
-//    private void initTabHost() {
-//        tabHost = (TabHost) findViewById(R.id.tabHost);
-//        tabHost.setup();
-//
-//        String[] tabNames = {"Home", "Information", "Visual"};
-//
-//        for(int i = 0; i < tabNames.length; i++){
-//            TabHost.TabSpec tabSpec;
-//            tabSpec = tabHost.newTabSpec(tabNames[i]);
-//            tabSpec.setIndicator(tabNames[i]);
-//            tabSpec.setContent(new FakeContent(getApplicationContext()));
-//            tabHost.addTab(tabSpec);
-//        }
-//        tabHost.setOnTabChangedListener(this);
-//    }
-
-//    private void initViewPagers(){
-//        viewPager = (ViewPager)findViewById(R.id.view_pager);
-//
-//        List<Fragment> listFragments =  new ArrayList<Fragment>();
-//        listFragments.add(new HomeFragment());
-//        listFragments.add(new InformaionFragment());
-//        listFragments.add(new VisualizationFragment());
-//        MyFragmentPagerAdapter myFragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager(), listFragments);
-//        viewPager.setAdapter(myFragmentPagerAdapter);
-//        viewPager.setOnPageChangeListener(this);
-//    }
 }
