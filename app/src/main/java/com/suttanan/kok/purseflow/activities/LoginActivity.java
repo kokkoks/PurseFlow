eebb71a418f4e4534b61f53d4e64ff83f261afde
kokkoks <kok_satan@hotmail.com>
2016-05-11 10:50:00 +0700

    add facebook login

--- app/src/main/java/com/suttanan/kok/purseflow/activities/LoginActivity.java
+++ app/src/main/java/com/suttanan/kok/purseflow/activities/LoginActivity.java
@@ -2,22 +2,48 @@ package com.suttanan.kok.purseflow.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.suttanan.kok.purseflow.R;
+
+//import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by KOKKOK on 5/4/2016.
  */
-public class LoginActivity extends AppCompatActivity{
+public class LoginActivity extends FragmentActivity{
 
-    public void LoginActivity(){
-
-    }
+    CallbackManager callbackManager;
+    LoginButton loginButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>(){
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
     }
 }
