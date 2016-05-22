12bc41ec89b4d05d8bab979d2141022f3e2976d2
kokkoks <kok_satan@hotmail.com>
2016-05-22 13:22:29 +0700

    set portrait

--- app/src/main/java/com/suttanan/kok/purseflow/activities/MainActivity.java
+++ app/src/main/java/com/suttanan/kok/purseflow/activities/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
         initTabs();
